90da75c33a6c48a552ba3ec11b9c5698ab880046
doubleOZ <monsterdaniel97@gmail.com>
2022-09-28 10:37:20 +0800

    Done: Day 01

---
+++ 01_Day_Introduction/follow_along/01_Day_Introduction/main.js
@@ -0,0 +1,20 @@
+//comments can make code readable
+
+// Welcome to 30DaysofJavaScript
+
+/* comments can make code readable,
+easy to reuse and informative
+*/
+
+console.log(firstName, lastName, country, city, age, isMarried);
+
+// Declaring variables with number values
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in °C
+const PI = 3.14; // geometrical constant
+console.log(gravity, boilingPoint, PI);
+// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+let name = "Asabeneh",
+  job = "teacher",
+  live = "Finland";
+console.log(name, job, live);
